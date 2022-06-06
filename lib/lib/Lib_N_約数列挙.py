bec7c8abf275192bd43b4e2375e7571189f41080
ibtosmlin <ibt.osm@gmail.com>
2022-06-07 07:58:05 +0900

    primes

---
+++ lib/lib/Lib_N_約数列挙.py
@@ -0,0 +1,25 @@
+#name#
+# 約数列挙
+#descripiton#
+# 約数列挙
+#body#
+##############################
+# 約数列挙 O(n**0.5)
+# returns sorted list
+##############################
+def make_divisors(n:int) -> list:
+    lower_divisors, upper_divisors = [], []
+    for i in range(1, int(n**0.5)+1):
+        if n % i != 0: i += 1; continue
+        lower_divisors.append(i)
+        j = n // i
+        if i != j: upper_divisors.append(j)
+    return lower_divisors + upper_divisors[::-1]
+
+#####################
+print(make_divisors(10))
+#####################
+
+#prefix#
+# Lib_約数列挙
+#end#
---
+++ lib/lib/Lib_N_素数判定.py
@@ -0,0 +1,41 @@
+#name#
+# 素数判定・出力
+#descripiton#
+# 素数判定・出力
+#body#
+
+##############################
+# 素数判定 O(n**0.5)
+##############################
+def is_prime(n:int) -> bool:
+    if n in {0, 1}: return False
+    if n == 2: return True
+    if n % 2 == 0: return False
+    for i in range(3, int(n**0.5)+1, 2):
+        if n % i == 0: return False
+    return True
+
+#####################
+print(10, is_prime(10))
+print(31, is_prime(31))
+#####################
+
+
+##############################
+# 素数出力 O(n**0.5)
+##############################
+def get_primes(n:int) -> list:
+# n以下の素数列挙
+    n += 1
+    IsPrime = [True] * n
+    IsPrime[0], IsPrime[1] = False, False
+    for p in range(n):
+        if not IsPrime[p]: continue
+        for j in range(p*2, n, p):
+            IsPrime[j] = False
+    ret = [p for p in range(n) if IsPrime[p]]
+    return ret
+
+#prefix#
+# Lib_prime_素数
+#end#
