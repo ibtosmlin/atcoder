9ab427d8cbea43bf749d0492fa3fe239b406c997
ibtosmlin <ibt.osm@gmail.com>
2022-08-23 08:30:45 +0900

    20220823

---
+++ work/arc089_a.py
@@ -0,0 +1,35 @@
+import sys
+from itertools import *
+from operator import itemgetter
+from collections import defaultdict, Counter, deque
+from heapq import heapify, heappop, heappush
+from functools import lru_cache
+sys.setrecursionlimit(10001000)
+INF = float('inf')
+mod = 1000000007; mod1 = 998244353
+PI = 3.141592653589793
+ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alps = 'abcdefghijklmnopqrstuvwxyz'
+def modinv(x, mod): return pow(x, mod - 2, mod)
+def input(): return sys.stdin.readline().rstrip()
+def int1(x): return int(x)-1
+def alp(i): return chr(ord('a') + i%26)    # i=0->'a', i=25->'z'
+def end(r=-1): print(r); exit()
+direc = [(1, 0), (0, 1), (-1, 0), (0, -1)] + [(1, 1), (1, -1), (-1, 1), (-1, -1)]
+def isinhw(i, j, h, w): return (0 <= i < h) and (0 <= j < w)
+n = int(input())
+x = 0
+y = 0
+t = 0
+for _ in range(n):
+    nt, nx, ny = map(int, input().split())
+    d = (nt - t) - (abs(nx-x) + abs(ny-y))
+    if d >=0 and d%2==0:
+        t = nt
+        x = nx
+        y = ny
+    else:
+        end('No')
+
+
+end('Yes')
---
+++ work/arc146_a.py
@@ -0,0 +1,35 @@
+import sys
+from itertools import *
+from operator import itemgetter
+from collections import defaultdict, Counter, deque
+from heapq import heapify, heappop, heappush
+from functools import lru_cache
+sys.setrecursionlimit(10001000)
+INF = float('inf')
+mod = 1000000007; mod1 = 998244353
+PI = 3.141592653589793
+ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alps = 'abcdefghijklmnopqrstuvwxyz'
+def modinv(x, mod): return pow(x, mod - 2, mod)
+def input(): return sys.stdin.readline().rstrip()
+def int1(x): return int(x)-1
+def alp(i): return chr(ord('a') + i%26)    # i=0->'a', i=25->'z'
+def end(r=-1): print(r); exit()
+direc = [(1, 0), (0, 1), (-1, 0), (0, -1)] + [(1, 1), (1, -1), (-1, 1), (-1, -1)]
+def isinhw(i, j, h, w): return (0 <= i < h) and (0 <= j < w)
+n = int(input())
+a = list(input().split())
+ta = []
+for ai in a:
+    ta.append((len(ai) , int(ai)))
+ta.sort(reverse=True)
+tat = [str(y) for x, y in ta[:3]]
+
+ret = 0
+ret = max(ret, int(tat[0]+tat[1]+tat[2]))
+ret = max(ret, int(tat[0]+tat[2]+tat[1]))
+ret = max(ret, int(tat[1]+tat[2]+tat[0]))
+ret = max(ret, int(tat[1]+tat[0]+tat[2]))
+ret = max(ret, int(tat[2]+tat[0]+tat[1]))
+ret = max(ret, int(tat[2]+tat[1]+tat[0]))
+print(ret)
