92a89beef754bf4f8a056678240439c277b53b17
ibtosmlin <ibt.osm@gmail.com>
2022-12-12 20:02:16 +0900

    20221212

--- work/tessoku_book_bx.py
+++ work/tessoku_book_bx.py
@@ -1,25 +1,20 @@
 # https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bx
-
-
+from bisect import bisect_left, bisect_right
+mod = 10**9 + 7
 n, w, l, r = map(int, input().split())
 x = [0] + list(map(int, input().split())) + [w]
-dp = [0] * (n+2)
+N = len(x)
+dp = [0] * N
 dp[0] = 1
-ndp = [0] * (n+3)
-ndp[1] = 1
 
-pl, pr = 0, 0
 
-for i, xi in enumerate(x):
-    if i == 0: continue
-    while pl < i and not (l <= xi - x[pl] <= r):
-        pl += 1
-    pr = pl
-    while pr < i and (l <= xi - x[pr] <= r):
-        pr += 1
-    dp[i] = ndp[pr] - ndp[pl]
-    ndp[i+1] += dp[i] + ndp[i]
-    print(pr, pl)
+for i in range(1, N):
+    xi = x[i]
+    rp = bisect_right(x, xi - l) - 1
+    lp = bisect_left(x, xi - r)
+#    print(lp, rp,xi, xi-r, xi-l)
+    dp[i] = rdp[rp] - rdp[lp]
+    dp[i] %= mod
+    rdp.append((rdp[-1]+dp[i])%mod)
+
 print(dp[-1])
-print(dp)
-print(ndp)
