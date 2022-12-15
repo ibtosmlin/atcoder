ee729eec614d9166ffcd7a25ed06f00c670391b4
ibtosmlin <ibt.osm@gmail.com>
2022-12-15 20:13:52 +0900

    20221214

--- work/tessoku_book_bt.py
+++ work/tessoku_book_bt.py
@@ -1,30 +1,10 @@
 # https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bt
-import sys
-from itertools import *
-from operator import itemgetter
-from collections import defaultdict, Counter, deque
-from heapq import heapify, heappop, heappush
-from functools import lru_cache
-sys.setrecursionlimit(10001000)
-INF = float('inf')
-mod = 1000000007; mod1 = 998244353
-PI = 3.141592653589793
-ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-alps = 'abcdefghijklmnopqrstuvwxyz'
-def modinv(x, mod): return pow(x, mod - 2, mod)
-def input(): return sys.stdin.readline().rstrip()
-def int1(x): return int(x)-1
-def alp(i): return chr(ord('a') + i%26)    # i=0->'a', i=25->'z'
-def end(r=-1): print(r); exit()
-direc = [(1, 0), (0, 1), (-1, 0), (0, -1)] + [(1, 1), (1, -1), (-1, 1), (-1, -1)]
-def isinhw(i, j, h, w): return (0 <= i < h) and (0 <= j < w)
-def dist2(pt1, pt2): return sum([(x1-x2) ** 2 for x1, x2 in zip(pt1, pt2)])
 h, w, k = map(int, input().split())
 c = list(input() for _ in range(h))
-d = [[0]*w for _ in range(h)]
+d = [[0] * w for _ in range(h)]
 for i in range(h):
     for j in range(w):
-        if c[i][j] == '.': d[i][j] = 1
+        if c[i][j] == '#': d[i][j] = 1
 
 ret = 0
 for s in range(1<<h):
@@ -32,7 +12,12 @@ for s in range(1<<h):
     other = []
     for i in range(h):
         if s >> i & 1:
-            now += w
-        else:
-            other.append(d[i])
-            
+            now += 1
+    for j in range(w):
+        now2 = 0
+        for k in range(h):
+            if s >> k & 1 == 0:
+                now2 += d[k][j]
+        other.append(now2)
+    other.sort()
+    other[:k-]
