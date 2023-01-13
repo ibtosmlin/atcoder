e36683f0cb64cc690ccd0db7865de6a4b0ce3eba
ibtosmlin <ibt.osm@gmail.com>
2023-01-13 11:53:15 +0900

    20230113

--- lib/lib/Lib_C_Hash.py
+++ lib/lib/Lib_C_Hash.py
@@ -4,13 +4,12 @@
 #body#
 # https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bd
 
-class RollingHash():
-    def __init__(self, s:str, base, mod):
+class RollingHash:
+    def __init__(self, s:str, base=31, mod=10**9+7):
         self.mod = mod
         self.pw = pw = [1]*(len(s)+1)
         self.length = l = len(s)
         self.h = h = [0]*(l+1)
-
         v, t = 0, 1
         for i in range(l):
             v, t = v * base, t * base
@@ -19,15 +18,12 @@ class RollingHash():
             h[i+1], pw[i+1] = v, t
 
     def get(self, l, r):
-        # returns hashvalue of [l, r]
-        if l > r: return False
-        r = min(self.length, r)
-        return (self.h[r] - self.h[l-1] * self.pw[r-l+1]) % self.mod
+        # returns hashvalue of [l, r)
+        return (self.h[r] - self.h[l] * self.pw[r-l]) % self.mod
 
-base = 31; mod = 10**9+7
-n, q = map(int, input().split())
-S = input()
-RHS = RollingHash(S, base, mod)
+q = int(input())
+s = input()
+RHS = RollingHash(s)
 
 for _ in range(q):
     a, b, c, d = map(int, input().split())
--- work/abc284_f.py
+++ work/abc284_f.py
@@ -1,36 +1,32 @@
+class RollingHash():
+    def __init__(self, s:str, base=31, mod=10**9+7):
+        self.mod = mod
+        self.pw = pw = [1]*(len(s)+1)
+        self.length = l = len(s)
+        self.h = h = [0]*(l+1)
+        v, t = 0, 1
+        for i in range(l):
+            v, t = v * base, t * base
+            v += ord(s[i])
+            v, t = v % mod, t % mod
+            h[i+1], pw[i+1] = v, t
+
+    def get(self, l, r):
+        # returns hashvalue of [l, r)
+        return (self.h[r] - self.h[l] * self.pw[r-l]) % self.mod
+
 n = int(input())
 m = 2*n
-t = input()
-counts = [[0] * 26 for _ in range(m+1)]
+T = input()
 
-for i, ti in enumerate(t):
-    j = ord(ti) - ord('a')
-    counts[i+1][j] += 1
+RHF = RollingHash(T[:n])
+RHS = RollingHash(T[n:][::-1])
 
-for i in range(m):
-    for j in range(26):
-        counts[i+1][j] += counts[i][j]
-
-rc = [0] * 26
-for j in range(26):
-    if counts[-1][j]%2:
-        print(-1)
-        exit()
-    else:
-        rc[j] = counts[-1][j] // 2
-
-#print(rc)
-
-for l in range(m-n):
-    r = l + n
-    rcn = [0] * 26
-    for j in range(26):
-        rcn[j] = counts[r][j] - counts[l][j]
-    if rcn != rc:
-        continue
-    nt = t[l:r][::-1]
-    if t[:l] + t[r:] == nt:
-        print(nt)
+for l in range(n+1):
+    hashF = (RHF.get(0, l), RHF.get(l, n))
+    hashS = (RHS.get(n-l, n), RHS.get(0, n-l))
+    if hashF == hashS:
+        print(T[:l]+T[l:n][::-1])
         print(l)
         exit()
 print(-1)
