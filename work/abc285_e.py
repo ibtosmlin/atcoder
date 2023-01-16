78fb199bee8856f216ca305bc7c0b26bb1ed8bc6
ibtosmlin <ibt.osm@gmail.com>
2023-01-17 07:53:15 +0900

    20230117

---
+++ work/abc285_e.py
@@ -0,0 +1,25 @@
+# https://atcoder.jp/contests/abc285/tasks/abc285_e
+from itertools import *
+from operator import itemgetter
+from collections import defaultdict, Counter, deque
+from heapq import heapify, heappop, heappush
+from functools import lru_cache
+import sys
+sys.setrecursionlimit(10001000)
+INF = float('inf'); INF1 = 10 ** 9
+mod = 1000000007; mod1 = 998244353
+PI = 3.141592653589793
+direc = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+direc8 = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+yes = 'Yes'; no = 'No'
+ALPS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alps = 'abcdefghijklmnopqrstuvwxyz'
+def alp(i): return chr(ord('a') + i%26)    # i=0->'a', i=25->'z'
+def modinv(x, mod): return pow(x, mod - 2, mod)
+def input(): return sys.stdin.readline().rstrip()
+def int1(x): return int(x)-1
+def notisinhw(i, j, h, w): return not ((0 <= i < h) and (0 <= j < w))
+def end(r=-1): print(r); exit()
+n = int(input())
+a = list(map(int, input().split()))
+dp = [[]*(n) for _ in range(n+1)]
